f48b025783c9f0556e42151905dbb1e4ed9fbf41
nadia <47458652+Nadia214@users.noreply.github.com>
2022-06-06 12:14:56 +0600

    Update 138. Copy List with Random Pointer.cpp

--- week05/138. Copy List with Random Pointer.cpp
+++ week05/138. Copy List with Random Pointer.cpp
@@ -1,41 +1,43 @@
 //138. Copy List with Random Pointer
 //Tc - O(N)
 //Sc - O(1)
-class Solution {
+class Solution
+{
 public:
-    Node* copyRandomList(Node* head) {
+    Node* copyRandomList(Node* head)
+    {
         if(!head)return head;
-		Node* curr = head;
-		while(curr)
-		{
-          Node* newNode = new Node(curr -> val);
-		  newNode->next = curr ->next;
-		  curr->next = newNode;
-		  curr = newNode->next;
+        Node* curr = head;
+        while(curr)
+        {
+            Node* newNode = new Node(curr -> val);
+            newNode->next = curr ->next;
+            curr->next = newNode;
+            curr = newNode->next;
         }
-		curr = head;
-		while(curr)
-		{
-          if(curr -> random)
-		  {
-             curr->next->random = curr->random->next;
-		  }
-		  curr = curr->next->next;
-		}
-	    Node* result =  head->next;
+        curr = head;
+        while(curr)
+        {
+            if(curr -> random)
+            {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        Node* result =  head->next;
         Node* copy = head->next;
         Node* original =head;
 
         while(original  && copy)
         {
-           Node* curr = copy->next;
-           original->next = curr;
-           if(curr)
-           {
-             copy->next = curr->next;
-           }
-           original = original->next;
-           copy = copy->next;
+            Node* curr = copy->next;
+            original->next = curr;
+            if(curr)
+            {
+                copy->next = curr->next;
+            }
+            original = original->next;
+            copy = copy->next;
         }
         return result;
     }
